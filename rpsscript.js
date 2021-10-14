b5c3ba33955c11bf6a542b5981aaa13bd77fb26b
mattinatux <berlinbullet@gmail.com>
2021-10-14 09:11:45 -0400

    add game function, move user input, and put in a few alerts

--- rpsscript.js
+++ rpsscript.js
@@ -32,6 +32,9 @@ function playRPS(playerSelection, computerSelection) {
     // should take input, do the comparison with the computerplay
     // and return "You <Win/Lose>! <ELEMENT> beats <ELEMENT>"
 
+    // decided to ask the player in this function so we can play multiple games :-)
+    playerSelection = playerSelection || prompt("What say you, chap?\n\nRock, Paper, or Scissors?","Type your choice here");
+
     // 1. Declare my variables: winner, elementWin, elementLose
     // Decided to declare them as globals b/c scope. Was that necessary?
 
@@ -65,12 +68,15 @@ function setWinningElements(whoWon, playerSelection, computerSelection) {
     if (whoWon == "Win") {
         elementWin = playerSelection;
         elementLose = computerSelection;
+        alert('You ' + whoWon + '!\n\nYou chose:' + playerSelection + '\nComputer chose: ' + computerSelection);
     } else if (whoWon == "Lose") {
         elementWin = computerSelection;
         elementLose = playerSelection;
+        alert('You ' + whoWon + '!\n\nYou chose: ' + playerSelection + '\nComputer chose: ' + computerSelection);
     } else if (whoWon == "Tie") {
         elementWin = "Nothing";
         elementLose = "anything in a tie!";
+        alert('You ' + whoWon + '!\n\nYou chose: ' + playerSelection + '\nComputer chose: ' + computerSelection);
     } else {
         console.log('Error in setWinningElements logic');
     }
@@ -84,13 +90,45 @@ function setWinningElements(whoWon, playerSelection, computerSelection) {
 // Declaring global variables to be used in playRPS
 // and setWinningElements
 // and in output to user
+let playerSelection;
 let winOrLose;
 let elementWin;
 let elementLose;
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
 // Get input from user and invoke the playRPS game
-let userInput = prompt("What say you, chap?\n\nRock, Paper, or Scissors?","Type your choice here");
+// MOVING TO INSIDE THE FUNCTION
+// let userInput = prompt("What say you, chap?\n\nRock, Paper, or Scissors?","Type your choice here");
 
 // Testing the playRPS and setWinningElements functions
 // console.log(playRPS(userInput,computerPlay()));
 // console.log("You " + winOrLose + ". " + elementWin + " beats " + elementLose);
+
+function game(rounds) {
+    for (let i = 1; i <= parseInt(rounds); i++) {
+        // play the game
+        playRPS(playerSelection,computerPlay());
+        
+        // tally it up each round
+        if (winOrLose == "Win") {
+            playerScore++;
+        } else if (winOrLose = "Tie") {
+            tieScore++;
+        } else if (winOrLose == "Lose") {
+            computerScore++;
+        }
+    }
+
+    // return an array with the player score first and computer score second
+    return [playerScore,computerScore];
+}
+
+// Testing the game
+//console.log(game(2));
+let rounds = prompt('How many games would you like, pal?','Enter a number here')
+    //invoke it with the rounds chosen
+    game(rounds);
+    //give the results of the match (game end)
+    alert("The results are in!\n\n" + "You scored: " + playerScore + "\n\nThe computer scored: " + computerScore + "\n\nAnd you tied " + tieScore + " time(s)!");
